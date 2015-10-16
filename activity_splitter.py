3a0d6ae19803d77713875376d9e9302a24f6b499
lay-z <priyavrocks@gmail.com>
2015-10-16 12:14:45 +0100

    Split up steps into sections

--- activity_splitter.py
+++ activity_splitter.py
@@ -1,10 +1,13 @@
 import json
+import re
 
 def split_activities(section):
     test2 = section.split('\n+ ')
     for part in test2:
         
-    
+
+pattern = re.compile(r```blocks[^`]*```,test2)
+re.match(r```blocks[^`]*```,test2)
 
 testfile = open("act-test.md", "r").read()
 split_activities(testfile)
@@ -38,3 +41,60 @@ split_activities(testfile)
     
 #     file = open("Flappy Parrot.md", "r").read()
 #     json_file = open("test.json", 'w')
+#```blocks[^`]*```
+
+import re
+
+DEBUG = False
+
+def parseComponent(text):
+	'''Parse component takes a block of text and returns an object consisting of
+		text
+		blocks
+		image
+	'''
+
+	result = {}
+
+	blockPattern = re.compile(r'```blocks([^`]*)```')
+
+	# find blocks
+	match = blockPattern.search(text)
+	if (match is not None):
+		result['blocks'] = match.group(1).strip()
+		if DEBUG: print('blocks:',result['blocks'])
+
+	# remove blocks
+	text = blockPattern.sub('',text)
+	# print('text',text)
+
+	photoPattern = re.compile(r'!\[screenshot\]\((.*)\)')
+
+	# find photos
+	match = photoPattern.search(text)
+	if (match is not None):
+		result['photo'] = match.group(1).strip()
+		if DEBUG: print('photo:',result['photo'] )
+
+	# remove photos
+	result['text']  = photoPattern.sub('',text).strip()
+	if DEBUG: print('text:',result['text'])
+
+	return result
+
+
+if (__name__ == "__main__"):
+
+	test = """
+Give Flappy the following script:
+```blocks
+    when FLAG clicked
+        go to x: (-50) y: (0)
+        forever
+            change y by (-3)
+```
+![screenshot](pipe_design.png)
+	"""
+
+	DEBUG = True
+	parseComponent(test)
--- sections.py
+++ sections.py
@@ -2,13 +2,13 @@ import json
 import re
 
     
-def split_sections(file_content):
+def split_project(file_content):
     '''
+    Splits a CodeClub project into respective steps
     @params file_content: .md file being split up into sections expects it to already
-                        have been opened
+                        have been opened and read
     @return dictionary: will return dictionary with section names as keys
                         values will be file objects of respective sections
-    Splits a file into its respective sections
     '''
     dictionary = {}
     # for i,step in enumerate(file.split('\n# ')):
@@ -21,7 +21,7 @@ def parse_step(step):
     @params step: string object of a step to parse step information from
     @return dictionary
     '''
-# \n# .*
+
     step_info = re.compile(r'\*\*Step (\d+):\*\* ([^\{]*)\{*.*\}*([^#]*)')
     info_matches = step_info.search(step)
     # # description_info = re.compile(r'\}\n\n(.*)\n\n## ', re.DOTALL) # DOTALL used so that '.' also matches '\n'
@@ -47,6 +47,36 @@ def get_heading(intro_section):
     if matches is not None:
         return matches.group(1).strip()
 
+def parse_project(intro_section):
+    '''
+    Gets the project_description, project_image, and project_title in a 
+    dictionary. 
+    @param intro_section: Data 
+    @return dictionary of project_description, project_image and project_title
+    '''
+
+def split_step(step):
+    '''
+    Splits up a step into into its respective components (e.g. activity check list etc)
+    The Description of the step is removed from the returned list
+    @param step: Step of a CodeClub Project
+    @return list of steps split up (excluding description)
+    '''
+    return step.split('\n## ')[1:]
+
+def parse_step_section(step_section):
+    '''
+    Parses through step section and returns dictionary with header and data
+    @param step_section: section within step
+    @return dictionary containing step header and step data
+    '''
+    regex = re.compile(r'(.*)\{ \.\w{1}([]*)')
+    matches = regex.search(step_section)
+    if matches is not None:
+        return {
+            'header': matches.group(1),
+            'body': matches.group(2),
+        }
 
 if __name__ == '__main__':
     #This code will when not being imported in another file
@@ -60,10 +90,14 @@ if __name__ == '__main__':
     print get_heading(intro)
 
     # test parse_step
-    step_info = open("flappy_parrot/step3.md", "r").read()
-    # print step_info
+    step_info = open("flappy_parrot/step7.md", "r").read()
 
+    # print step_info
     print parse_step(step_info)
+
+    # test split_step
+    # for step in split_step(step_info):
+    #     print parse_step_section(step)
     
     
     

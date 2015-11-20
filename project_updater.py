7d87e9279e7855616785c7d8106de23b41ab8185
Henry Miskin <henry.miskin@talktalk.net>
2015-11-20 11:19:31 +0000

    Completed code pipeline to apply corrections to projects

--- project_updater.py
+++ project_updater.py
@@ -5,27 +5,41 @@ import config
 import project_finder
 import database_writer
 
-environment = os.environ.get('PYTHON_ENV','development')
-mongolabUri = 'mongodb://localhost:27017/turing_development'
+def main():
+    environment = os.environ.get('PYTHON_ENV','development')
+    mongolabUri = 'mongodb://localhost:27017/turing_development'
 
-if (environment == 'production'):
-    mongolabUri = config.mongolabUri
+    if (environment == 'production'):
+        mongolabUri = config.mongolabUri
 
-codeclubProjects = []
-for project in project_finder.find_projects():
-    if (project['title'] not in config.projects['ignore']):
-        codeclubProjects.append(project)
+    codeclubProjects = []
+    for project in project_finder.find_projects():
+        if (project['title'] not in config.projects['ignore']):
+            codeclubProjects.append(project)
 
-json.dump(codeclubProjects,open('data/projects_codeclub.json','w'),indent=4)
-turinglabProjects = json.load(open('data/projects_turinglab.json'))
-projects = turinglabProjects + codeclubProjects
+    json.dump(codeclubProjects,open('data/projects_codeclub.json','w'),indent=4)
+    turinglabProjects = json.load(open('data/projects_turinglab.json'))
+    projects = turinglabProjects + codeclubProjects
+
+    corrections = json.load(open('data/corrections.json'))
+    for project in projects:
+        projectCorrections = corrections[project['title']]
+
+        for correction in projectCorrections:
+            exec('project' + correction, globals(), {'project':project})
 
 
-databaseWriter = database_writer.DatabaseWriter('projects',mongolabUri)
+    json.dump(projects,open('data/projects.json','w'),indent=4)
+    databaseWriter = database_writer.DatabaseWriter('projects',mongolabUri)
 
-print('Updating {} Database'.format(environment.title()))
-print('='*80)
+    return
 
-for project in projects:
-    updated = databaseWriter.update({ 'title' : project['title'] },project)
-    print(updated.get('title',''),updated.get('link',''))
+    print('Updating {} Database'.format(environment.title()))
+    print('='*80)
+
+    for project in projects:
+        updated = databaseWriter.update({ 'title' : project['title'] },project)
+        print(updated.get('title',''),updated.get('link',''))
+
+if (__name__ == "__main__"):
+    main()

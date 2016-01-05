6617ffca6d2ead85a84fa03e03bf351a48d8ad65
Henry Miskin <henry.miskin@talktalk.net>
2016-01-05 17:08:11 +0000

    Updated project updator to only change certain projects

--- project_updater.py
+++ project_updater.py
@@ -12,32 +12,19 @@ def main():
     if (environment == 'production'):
         mongolabUri = config.mongolabUri
 
-    codeclubProjects = []
-    for project in project_finder.find_projects():
-        if (project['title'] not in config.projects['ignore']):
-            codeclubProjects.append(project)
-
-    json.dump(codeclubProjects,open('data/projects_codeclub.json','w'),indent=4)
-    turinglabProjects = json.load(open('data/projects_turinglab.json'))
-    projects = turinglabProjects + codeclubProjects
-
-    corrections = json.load(open('data/corrections.json'))
-    for project in projects:
-        projectCorrections = corrections[project['title']]
-
-        for correction in projectCorrections:
-            exec('project' + correction, globals(), {'project':project})
-
-
-    json.dump(projects,open('data/projects.json','w'),indent=4)
     databaseWriter = database_writer.DatabaseWriter('projects',mongolabUri)
 
     print('Updating {} Database'.format(environment.title()))
     print('='*80)
 
-    for project in projects:
+    for project in project_finder.find_projects():
+
+        if (project['title'] not in config.projects):
+            print('  -  {title}'.format(**project))
+            continue
+
         updated = databaseWriter.update({ 'title' : project['title'] },project)
-        print(updated.get('title',''),updated.get('link',''))
+        print('{title}'.format(**updated))
 
 if (__name__ == "__main__"):
     main()

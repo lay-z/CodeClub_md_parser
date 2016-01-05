d2198ef3440cff8211144deafa03a45668c92e69
Henry Miskin <henry.miskin@talktalk.net>
2016-01-05 16:58:28 +0000

    Updated project finder

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
 
-
-codeClub = {
-	'root' : 'code_club',
-	'language': 'en-GB',
-	'folders': ['Term 1','Term 2','Additional Projects','Archived Projects']
-	}
-
-projects = {
-	'root' : 'https://raw.githubusercontent.com/CodeClub/scratch-curriculum/master/',
-	'ignore' : ['Fireworks','Project Template','Christmas Capers','Create Your Own Game','Create Your Own World']
-	}
+imageRoot = 'https://s3-eu-west-1.amazonaws.com/turing-resources/'
 
 mongolabUri = 'mongodb://heroku_dh1386kx:5ki9curvtt1r4bsunu3893k3rh@ds033754.mongolab.com:33754/heroku_dh1386kx'
--- project_writer.py
+++ project_writer.py
@@ -12,6 +12,8 @@ def main():
     for project in projects:
 
         project['name'] = re.sub(r'\W','_',project['title'].lower())
+        project['show'] = False
+
         filename = 'projects/{number:02.0f}_{name}.json'.format(**project)
         json.dump(project,open(filename,'w'),indent=4,sort_keys=True)
 

3d34ff28896d20c6280ad55ce9141d829f1fa357
Henry Miskin <henry.miskin@talktalk.net>
2016-01-29 16:17:14 +0000

    Added ball movement to project

--- projects/web/drawing_with_canvas/solution/script.js
+++ projects/web/drawing_with_canvas/solution/script.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext('2d')
 
 function draw() {
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.arc(150, 150, 100, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
